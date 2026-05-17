2b0d2f1790c6ea5639c405672b11b9e0500bb2a1
Nivedh E R <nivedher12@gmail.com>
2026-05-17 19:29:05 +0530

    feat(createMainContainer): created dom element and appended the main container

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import "./styles/index.css";
+import { createMainContainer } from "./ui/main/WeatherContainer.js";
 
-console.log("index");
+const DOM = {
+  container: {
+    main: document.querySelector("#container"),
+  },
+};
+
+DOM.container.main.append(createMainContainer());
